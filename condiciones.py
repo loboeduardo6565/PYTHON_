c898d8594af62e9811b922569c13801e0e62c513
Eduardo Lobo <loboeduardo6565@gmail.com>
2022-12-15 11:36:01 -0300

    sumando condicionales básicas

--- condiciones.py
+++ condiciones.py
@@ -3,9 +3,25 @@ saldo = 500
 if saldo >0:
     print("Puedes pagar")
 
-
+# Revisar si una condición es mayor a 
 balance = 0 
 if balance > 0:
     print ('Posee saldo positivo')
 else:
     print ('No posee saldo positivo')
+
+# Sumar likes 
+likes = 200
+if likes == 200:
+    print(f'Tienes {likes} likes')
+
+# Sumar likes 
+likes = 200
+if likes >= 200:
+    print(f'Tienes {likes} likes')
+
+# If con string 
+lenguajes = 'sas'
+if lenguajes == 'sas':
+    print(f'Debes aprender lenguaje {lenguajes}')
+
