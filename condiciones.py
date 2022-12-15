eb632b9c5581b3e9c66f246a2ab62f6c2f2aaeeb
Eduardo Lobo <loboeduardo6565@gmail.com>
2022-12-15 15:39:02 -0300

    Se agregan varios tipos de condicionales

--- condiciones.py
+++ condiciones.py
@@ -25,3 +25,39 @@ lenguajes = 'sas'
 if lenguajes == 'sas':
     print(f'Debes aprender lenguaje {lenguajes}')
 
+# If con string negativo
+lenguajes = 'python'
+if not lenguajes == 'sas':
+    print(f'El lenguaje es {lenguajes}')
+else:
+    print(f'El lenguaje es {lenguajes}')
+
+# Evaluar un Boolean
+#sing_on = True
+sing_on = False
+
+if sing_on:
+    print('Accediendo al sistema')
+else:
+    print('Debes iniciar sesión')
+
+# Evaluar si un elemento existe o no en una lista
+lenguajes = ["Python", "Java", "JS", "Kotlin", "SAS", "Bash"];
+name='SAS'
+if name in lenguajes:
+    print(f'{name} está en la lista')
+else: 
+    print(f'{name} no está en la lista')
+
+#IF anidados
+user_auth = False
+user_admin = False
+
+if user_auth:
+    if user_admin:
+        print('El usuario tiene permisos de administrador')
+    else:
+        print('El usuario tiene acceso básico al sistema')
+else:
+    print('El usuario no ha iniciado sesión')
+
