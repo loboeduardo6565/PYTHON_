e1fa44b90a2c8a122f922107363e4f07918df17f
Eduardo Lobo <loboeduardo6565@gmail.com>
2022-12-16 22:41:40 -0300

    Se agrega un ejemplo usando while pero aún no anda bien

---
+++ anotherwhile.py
@@ -0,0 +1,19 @@
+q1 = input('3 + 2 es igual a 5: \r\n')
+
+if q1.upper() == 'SI':
+        print(f'¡Correcto!')
+        puntos=1
+elif q1.upper() == 'NO': 
+        print(f'¡La respuesta es incorrecta!')
+        puntos=0
+else:
+    while (q1.upper() != 'SI') or (q1.upper() != 'NO'):
+        print(f'Se debe responder con SI O NO')
+        q1 = input('3 + 2 es igual a 5: \r\n')
+
+        if q1.upper() == 'SI':
+            print(f'¡Correcto!')
+            puntos=1
+        elif q1.upper() == 'NO': 
+            print(f'¡La respuesta es incorrecta!')
+            puntos=0
