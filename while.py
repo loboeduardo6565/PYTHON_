f2dc6a2509790b0935293b34a1b820ac86129fcb
Eduardo Lobo <loboeduardo6565@gmail.com>
2023-01-02 14:38:53 -0300

    Se agrega un nuevo archivo con un ejemplo de while

---
+++ while.py
@@ -0,0 +1,13 @@
+numero = 0
+
+#while numero <= 10: 
+#    print(numero)
+#    numero += 1 # incrementa en 1 para evitar el loop infinito
+
+
+while numero <= 10: 
+    if numero == 5:
+        break
+    else:
+        print(numero)
+    numero += 1 # incrementa en 1 para evitar el loop infinito
